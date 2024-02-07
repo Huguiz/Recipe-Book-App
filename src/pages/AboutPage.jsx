5cbf67c1c5d76ebc971d50b67bd16787f2ce7462
Alastair Longmuir <alastairlinwood@gmail.com>
2024-02-07 16:43:29 +0100

    editing AboutPage + CSS

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,32 @@
 import '../App.css'
+import team from '../assets/team-members.json';
+import githubIcon from '../assets/github.png';
+import linkedinIcon from '../assets/linkedin.png';
 
 function AboutPage () {
 
 	return (
 
-		<div className="AboutPage">
+		<div className="team-container">
 
-		<p>Info about Alastair and Hugo will go here.</p>
+		{team.map(member => {
+
+			return (
+				
+				<div className="team-card" key={member.id}>
+
+					<p>{member.name}</p>
+
+					<div>
+					<a href={member.github}><img className="site-icon" src={githubIcon} /></a>
+					<a href={member.linkedin}><img className="site-icon" src={linkedinIcon} /></a>
+					</div>
+
+				</div>
+				
+
+				)
+		})}
 
 		</div>
 
